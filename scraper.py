8eb5bec3162ec4b019c20c35703e13eeaab60036
ayoubkachkach <ayoub.kachkach@gmail.com>
2019-11-21 02:02:33 +0100

    Use generators to lazily load PDFs to avoid out-of-memory errors.

--- scraper.py
+++ scraper.py
@@ -44,49 +44,54 @@ def to_xml(root, tag_to_value):
 
     return root_node
 
+def read_pdfs(path):
+    for path2pdf in path.glob('*pdf'):
+        with open(path2pdf, 'rb') as pdf:
+            filename, _ = os.path.splitext(path2pdf.name)
+            yield filename, pdf.read()
 
-def pdf_to_pil(pdf_path, dpi=300):
+
+def pdf_to_pil(pdf, dpi=300):
     """Converts pdf in pdf_path to a list of PIL images.
     
     Args:
-        pdf_path: pathlib.Path object holding path to PDF to convert.
+        pdf: Bytes of PDF to convert.
         dpi: (int) Desired DPI of converted images.
     Returns:
         List of PIL images, each holding a page of the pdf.
     """
     logging.log(logging.INFO, "Converting pdf to images ...")
     s = time.time()
-    images = pdf2image.convert_from_path(pdf_path, dpi=dpi, fmt='png')
+    images = pdf2image.convert_from_bytes(pdf, dpi=dpi, fmt='png', thread_count=4)
     e = time.time()
     logging.log(logging.INFO, "took %.2fs." % (e - s))
     return images
 
 
-def get_images_from_pdfs(path):
-    """Generator that converts all PDFs in path to images.
-    args:
-        path: pathlib.Path object holding path to folder containing pdfs to convert.
-    returns:
-        Generator that yields list of images of pdf to convert in every call.
-    """
-    for path2pdf in path.glob('*pdf'):
-        print('hey')
-        images = pdf_to_pil(path2pdf)
-        filename, _ = os.path.splitext(path2pdf.name)
-        yield (filename, images)
-
-
 def add_pages(texts):
-    print('here')
     return [f'\n[p.{page_num+1}]\n{text}' for page_num, text in enumerate(texts)]
 
 
 def join_hyphenated_words(texts):
-    print('here')
     return [re.sub(r'-\n(\w+ *)', r'\1\n', text) for text in texts]
 
 
-def scrape(path, text_processors, lang='fra', threads=10):
+def extract_volume_info(tag_to_value):
+    vol, year = None, None
+    for tag, value in tag_to_value:
+        if tag != 'title':
+            continue
+        
+        m = re.match('Volume_(.*)_(.*)', value)
+        if not m:
+            return None
+        vol = m.group(1)
+        year = m.group(2)
+
+    return tag_to_value + (('vol', vol), ('year', year),)
+
+
+def scrape(path, text_processors, xml_constructors, lang='fra', threads=10):
     """Scrapes text from all pdfs in path. The text scraped is then given to functions in text_processors successively.
     args:
         path: pathlib.Path object holding path to folder containing pdfs to scrape.
@@ -94,15 +99,19 @@ def scrape(path, text_processors, lang='fra', threads=10):
             a list of processed texts.
         lang: string containing ISO 639-1 representation of language of the pdf to be scraped.
     returns:
-        XML root whose children ('document' tags) are the pdfs found in path. 
+        List of XML 'document' tags.
         Each document tag has 3 children: an artificial id, a title (filename) and the content
         of the pdf. Each text scraped is separated by an indicator of the page number the text was scraped from.
         The indicator is in the form "[p.X]" where x is the page number. This indicator appears at the end of
         the text from said page.
     """
-    document_tags = ET.Element('documents')
     try:
-        for document_id, (folder_name, images) in enumerate(get_images_from_pdfs(path)):
+        logging.log(logging.INFO, 'Reading PDFs ...')
+        for document_id, (filename, pdf) in enumerate(read_pdfs(path)):
+            logging.log(logging.INFO, f'Reading PDFs ...')
+            # Convert pdf to images
+            images = pdf_to_pil(pdf)
+
             logging.log(logging.INFO, "Running OCR on document %s " % document_id)
             s = time.time()
             with ThreadPoolExecutor(threads) as executor:
@@ -117,27 +126,31 @@ def scrape(path, text_processors, lang='fra', threads=10):
             logging.log(logging.INFO, "took %.2fs." % (e - s))
 
             # Construct XML from scraped documents.
-            tag_to_value = (('id', str(document_id)), ('title', clean_text(folder_name)), ('body', clean_text(body)))
-            document_tags.append(to_xml(root='document', tag_to_value=tag_to_value))
+            filename = clean_text(filename)
+            tag_to_value = (('id', str(document_id)), ('title', filename), ('body', clean_text(body)))
+            for xml_constructor in xml_constructors:
+                tag_to_value = xml_constructor(tag_to_value)
+
+            document_tag = to_xml(root='document', tag_to_value=tag_to_value)
+            tree = ET.ElementTree(document_tag)        
+            with open(f'{filename}.xml', 'wb') as f:
+                tree.write(f, encoding='utf-8', pretty_print=True)
+
+            logging.log(logging.INFO, f'Document {document_id + 1} done.')
 
     except KeyboardInterrupt:
         logging.log(logging.INFO, 'Program stopped by user ... returning XMLs created so far to file')
     finally:
-        return document_tags
+        return []
 
 
 if __name__ == '__main__':
     args = PARSER.parse_args()
 
     input_path = Path(args.path)
-    print(list(input_path.glob('*pdf')))
-    documents_tag = scrape(
+    document_tags = scrape(
         input_path,
         text_processors=[add_pages, join_hyphenated_words],
+        xml_constructors=[extract_volume_info],
         lang=args.lang,
         threads=args.threads)
-
-    tree = ET.ElementTree(documents_tag)
-    with open('output.xml', 'wb') as f:
-        tree.write(f, encoding='utf-8', pretty_print=True)
-    
