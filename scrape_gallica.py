da441b331a1ab086963490b370c2b9b4b9176844
ayoubkachkach <ayoub.kachkach@gmail.com>
2019-11-21 02:01:19 +0100

    Finalized download and crawling scripts

--- scrape_gallica.py
+++ scrape_gallica.py
@@ -16,7 +16,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import os
-
+import time
 
 def try_getting_element(driver, element, by):
     try:
@@ -24,9 +24,11 @@ def try_getting_element(driver, element, by):
             EC.element_to_be_clickable((by, element))
         ).click()
         # button.click()
-    except TimeoutException:
+    except Exception as e:
+        print(e)
         print('Found nothing!')
-        return None
+        driver.save_screenshot('screenshot_%s.png' % time.time())
+        return None        
 
     return button
 
@@ -39,18 +41,14 @@ def is_number(s):
         return False
 
 if __name__ == '__main__':
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(
-        '/home/ayoub/projects/revue-africane-ocr/', options=options)
-
+    driver = webdriver.PhantomJS('./phantomjs')
     root_node = ET.Element('documents')
+    driver.set_window_size(1900,1800)
 
     print("Getting RAW txt from links ...")
     df = pd.read_csv('revue_africaine_links.csv', sep=' ', index_col='volume')
     # Remove missing entries with missing links
     df = df[df['link'].str.startswith('https')]
-
     # Initialize HTML ids for selenium
     BUTTON_ID = 'icon-download'
     TXT_CHECKBOX_ID = 'texteBrut'
@@ -72,51 +70,22 @@ if __name__ == '__main__':
         print('Getting volume %s' % vol)
         link = row['link']
         driver.get(link)
-        time.sleep(zzz)
 
         # Get download icon and click
-        # download_icon = try_getting_element(driver, BUTTON_ID, By.ID)
-        download_icon = driver.find_element_by_id(BUTTON_ID)
-        if not download_icon:
-            print("Couldn't get volume %s ... moving on" % vol)
-            continue
-
-        download_icon.click()
+        download_icon = try_getting_element(driver, BUTTON_ID, By.ID)
 
         # Select text checkbox and click
-        # txt_button = try_getting_element(driver, TXT_CHECKBOX_ID, By.ID)
-        time.sleep(zzz)
-        txt_button = driver.find_element_by_id(TXT_CHECKBOX_ID)
-        if not txt_button:
-            print("Couldn't get volume %s ... moving on" % vol)
-            continue
+        txt_button = try_getting_element(driver, TXT_CHECKBOX_ID, By.ID)
 
-        txt_button.click()
-        time.sleep(zzz)
         # Select accept terms checkbox and click
-        # terms_checkbox = try_getting_element(driver, TERMS_CHECKBOX_ID, By.ID)
-        terms_checkbox = driver.find_element_by_id(TERMS_CHECKBOX_ID)
-        if not terms_checkbox:
-            print("Couldn't get volume %s ... moving on" % vol)
-            continue
-
-        terms_checkbox.click()
+        terms_checkbox = try_getting_element(driver, TERMS_CHECKBOX_ID, By.ID)
 
         # Select download button and click
-        # download_button = try_getting_element(driver, "//button[contains(.,'Download')]", By.XPATH)
-        time.sleep(zzz)
-        download_button = driver.find_element_by_xpath(
-            "//button[contains(.,'Download')]")
-        if not download_button:
-            print("Couldn't get volume %s ... moving on" % vol)
-            continue
+        download_button = try_getting_element(driver, "//button[contains(.,'Download')]", By.XPATH)
 
-        download_button.click()
-
-        # time.sleep(zzz)
         # Switch to newly opened window containing text to scrape
-        time.sleep(zzz * 5)
         driver.switch_to.window(driver.window_handles[1])
+        time.sleep(zzz * 4)
         # Get HTML of current window
         soup = bs4.BeautifulSoup(driver.page_source, "lxml")
         paragraphs = soup.find_all('p')
@@ -145,3 +114,5 @@ if __name__ == '__main__':
             tree.write(f, encoding='utf-8', pretty_print=True)
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
+
+    driver.close()
--- scrape_pdfs.py
+++ scrape_pdfs.py
@@ -6,8 +6,8 @@ import io
 import os
 from scraper import to_xml, scrape
 
-missing_vols = [str(i) for i in range(67, 107)]
-missing_vols += ['61', '55', '54', '51']
+missing_vols = [str(i) for i in range(99, 107)]
+missing_vols += ['51']
 
 ################################################################
 print("Getting PDFs from links ...")
@@ -16,16 +16,18 @@ html = requests.get(url).content
 soup = bs4.BeautifulSoup(html, 'lxml')
 # Get all links to folders
 for link in soup.findAll('a', attrs={'href': re.compile("[0-9]*_([0-9]*|fin)/")}):
-    if int(link.get('href').split('_')[0]) < 51:
+    start_vol = int(link.get('href').split('_')[0])
+    if start_vol != 51 and start_vol < 91:
         continue
 
     absolute_url = url + link.get('href')
     zip_page = requests.get(absolute_url).content
     soup = bs4.BeautifulSoup(zip_page, 'lxml')
     # Get all zip files in folder
-    for link in soup.findAll('a', attrs={'href': re.compile(r".*_(\d\d)\.zip")}):
+    for link in soup.findAll('a', attrs={'href': re.compile(r".*_\d{1,3}\.zip")}):
         zip_link = link.get('href')
-        vol = re.compile(r".*_\d{2,3}\.zip").match(zip_link).group(1).replace('.', '_')
+        vol = re.compile(r".*_(\d{2,3})\.zip").match(zip_link).group(1).replace('.', '_')
+        print('Downloading vol %s' % vol)
         if vol not in missing_vols:
             print('Skipping vol %s' % vol)
             continue
